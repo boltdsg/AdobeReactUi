7f03520e8187446be30bd8586e632b2ade4fd8ff
Samuel <boltcode17@gmail.com>
2020-05-19 13:52:36 +0300

    complete source code

--- src/Components/AuthBtn.js
+++ src/Components/AuthBtn.js
@@ -1,16 +1,94 @@
 import React from 'react';
 import {View, Text, StyleSheet} from 'react-native';
-import Animated from 'react-native-reanimated';
-import {bottomHeight, SvgBtnWidth} from '../../Variables';
+import Animated, {interpolate, SpringUtils} from 'react-native-reanimated';
+import {bottomHeight, SvgBtnWidth, SCREEN_HEIGHT} from '../../Variables';
 import YellowL from '../assets/yellowL.svg';
 import WhiteL from '../assets/whiteL.svg';
 import PropTypes from 'prop-types';
+import {TapGestureHandler} from 'react-native-gesture-handler';
+import {withSpringTransition} from 'react-native-redash';
 
-const AuthBtn = props => {
+const AuthBtn = ({hideandseek, gestureHandler}) => {
+  const bounceAnime = withSpringTransition(hideandseek.current, {
+    ...SpringUtils.makeDefaultConfig(),
+    overshootClamping: true,
+    damping: new Animated.Value(20),
+  });
+
+  const translateY = interpolate(bounceAnime, {
+    inputRange: [0, 0.5, 0.7, 1],
+    outputRange: [0, 0, -SCREEN_HEIGHT, -SCREEN_HEIGHT],
+  });
+
   return (
-    <View>
-      
-    </View>
+    <Animated.View
+      style={{
+        alignItems: 'center',
+        width: '100%',
+        height: bottomHeight,
+        paddingTop: 37,
+        transform: [{translateY}],
+      }}>
+      <Animated.View>
+        <TapGestureHandler {...gestureHandler}>
+          <Animated.View>
+            <Animated.View
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginVertical: 25,
+                justifyContent: 'center',
+              }}>
+              <View
+                style={{
+                  ...StyleSheet.absoluteFill,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <YellowL height={130} width={SvgBtnWidth} />
+              </View>
+              <Text
+                style={{
+                  fontFamily: 'Gilroy-ExtraBold',
+                  color: '#fff',
+                  fontSize: 23,
+                }}>
+                Register
+              </Text>
+            </Animated.View>
+
+            <Animated.View
+              style={{
+                display: 'flex',
+                alignItems: 'center',
+                marginVertical: 26,
+                justifyContent: 'center',
+              }}>
+              <View
+                style={{
+                  ...StyleSheet.absoluteFill,
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                }}>
+                <WhiteL height={130} width={SvgBtnWidth} />
+              </View>
+              <Text
+                style={{
+                  fontFamily: 'Gilroy-ExtraBold',
+                  color: '#000',
+                  fontSize: 23,
+                }}>
+                Login
+              </Text>
+            </Animated.View>
+          </Animated.View>
+        </TapGestureHandler>
+
+        <Text style={{fontFamily: 'Gilroy-Medium', fontSize: 15}}>
+          Read terms and conditions
+        </Text>
+      </Animated.View>
+    </Animated.View>
   );
 };
 
--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -1,11 +1,56 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
-import Animated from "react-native-reanimated";
-import { topHeight, SvgHeight, SvgWidth } from "../../Variables";
-import Illustrations from "../assets/illustration.svg";
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Animated, {interpolate} from 'react-native-reanimated';
+import {topHeight, SvgHeight, SvgWidth} from '../../Variables';
+import Illustrations from '../assets/illustration.svg';
+import {withTimingTransition} from 'react-native-redash';
 
-const Logo = (props) => {
-  return <View></View>;
+const Logo = ({zoom, hideandseek}) => {
+  const hidewithease = withTimingTransition(hideandseek.current);
+  const hidelogo = interpolate(hidewithease, {
+    inputRange: [0, 0.5, 0.7, 1],
+    outputRange: [1, 0, 0, 0],
+  });
+
+  return (
+    <Animated.View
+      style={{
+        height: topHeight,
+        justifyContent: 'center',
+        backgroundColor: '#fff',
+        paddingTop: 50,
+        opacity: hidelogo,
+      }}>
+      <Animated.View
+        style={{
+          justifyContent: 'center',
+          backgroundColor: '#fff',
+          paddingHorizontal: 10,
+        }}>
+        <Text
+          style={{
+            fontSize: 20,
+            textAlign: 'center',
+            fontFamily: 'Gilroy-Bold',
+            fontWeight: '300',
+            paddingHorizontal: 5,
+          }}>
+          Welcome to Gdcoin your smart cryto exchange
+        </Text>
+
+        <View>
+          <Animated.View
+            style={{
+              justifyContent: 'center',
+              alignItems: 'center',
+              transform: [{scale: zoom}],
+            }}>
+            <Illustrations height={SvgHeight} width={SvgWidth} />
+          </Animated.View>
+        </View>
+      </Animated.View>
+    </Animated.View>
+  );
 };
 
 Logo.propTypes = {};
